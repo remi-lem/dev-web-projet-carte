cdda1d8615e46f9ed1657810a1f2527a94b4940a
Rémi <contact@remilemaire.fr>
2023-12-04 15:14:03 +0100

    carte avec gares

---
+++ carte/leaflet.js
@@ -0,0 +1,29 @@
+
+let map = L.map('leafletMap').setView([46.980, 3.779], 6);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
+
+fetch("data/liste-des-gares.geojson")
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        L.geoJSON(data, {
+            onEachFeature: function (feature, layer) {
+                layer.bindPopup(feature.properties.libelle);
+            }
+        }).addTo(map);
+    })
+    .catch(function (error) {
+        console.error('Erreur lors du chargement des données GeoJSON :', error);
+    });
+fetch("data/fichier-de-formes-des-voies-du-reseau-ferre-national.geojson")
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        L.geoJSON(data).addTo(map);
+    })
+    .catch(function (error) {
+        console.error('Erreur lors du chargement des données GeoJSON :', error);
+    });
