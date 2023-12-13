4bb4dd9b2af386ed040a1c87fed173f5455e1ea7
Rémi <contact@remilemaire.fr>
2023-12-13 16:22:35 +0100

    avance compte

--- user_account.php
+++ user_account.php
@@ -2,8 +2,44 @@
 $titre = "Compte | GareÀVous";
 $urlstyle = "style/account.css";
 require_once("include/header.php"); // TODO : enlever le logo du compte utilisateur
+
+/*
+// METTRE CA SEULEMENT POUR LE DEBUG
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
+*/
+
+$envFilePath = __DIR__ . '/.env';
+$envContent = file_get_contents($envFilePath);
+$envVariables = parse_ini_string($envContent);
+
+$servername = $envVariables['DB_SERV'];
+$username = $envVariables['DB_USER'];
+$password = $envVariables['DB_PASS'];
+$dbname = $envVariables['DB_NAME'];
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT U.Name FROM User U WHERE U.Id = 1";
+$result = $conn->query($sql);
+
+// Process all rows
+while($row = mysqli_fetch_array($result)) {
+    $name = $row['Name'];
+}
+
+$conn->close();
+
 ?>
     <div id="congtenu-compte">
+        <h1>Bienvenue <?php echo($name)?> !</h1>
         <div id="favorite-gare">
             <h2>Mes gares préférées</h2>
             <ul>
